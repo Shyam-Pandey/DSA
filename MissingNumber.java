22f0a159f8ee81ddf928ffb11b7463307994e302
Shyam Pandey <shyampandey9971@gmail.com>
2023-04-05 12:22:01 +0530

    SingleNumber

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,17 +1,17 @@
 public class MissingNumber {
     public static int missingNumber(int[] nums) {
-        int size = nums.length;
+        int size = nums.length;// Find the size of array
         int xor1 = 0;
         int xor2 = 0;
         for (int i = 0; i < size; i++) {
-            xor2 = xor2 ^ nums[i];
-            xor1 = xor1 ^ (i + 1);
+            xor2 = xor2 ^ nums[i]; // Take xor of all array element such that 0^4^1^2^3
+            xor1 = xor1 ^ (i + 1); // Take xor of all number form 0 to n
         }
-        return xor1 ^ xor2;
+        return xor1 ^ xor2; // At final take xor of both xor1 and xor2
     }
 
     public static void main(String args[]) {
-        int nums[] = { 4, 1, 2, 3 };
+        int nums[] = { 4, 1, 2, 3, 0, 6 };
         int arr = missingNumber(nums);
         System.out.println(arr);
     }
